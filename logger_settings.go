81a638e088e2323d406b4d832909bd3468952ae3
Daniel Jr <daniel.souza.lima@outlook.com>
2025-03-05 16:27:25 -0300

    refactor: Update logger_settings to use ionservice for status checks

--- logger_settings.go
+++ logger_settings.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"log/slog"
 
-	"github.com/IonicHealthUsa/ionlog/internal/interfaces"
+	"github.com/IonicHealthUsa/ionlog/internal/ionservice"
 	"github.com/IonicHealthUsa/ionlog/internal/logcore"
 	"github.com/IonicHealthUsa/ionlog/internal/logrotation"
 )
@@ -14,7 +14,7 @@ type customAttrs func(i logcore.IIonLogger)
 // SetLogAttributes sets the log SetLogAttributes
 // fns is a variadic parameter that accepts customAttrs
 func SetLogAttributes(fns ...customAttrs) {
-	if logcore.Logger().Status() == interfaces.Running {
+	if logcore.Logger().Status() == ionservice.Running {
 		slog.Warn("Logger is already running, cannot set attributes")
 		return
 	}
@@ -49,8 +49,6 @@ func WithStaticFields(attrs map[string]string) customAttrs {
 // WithLogFileRotation enables log file rotation, specifying the directory where log files will be stored, the maximum size of the log folder in bytes, and the rotation frequency.
 func WithLogFileRotation(folder string, folderMaxSize uint, period logrotation.PeriodicRotation) customAttrs {
 	return func(i logcore.IIonLogger) {
-		i.SetRotationPeriod(period)
-		i.SetFolder(folder)
-		i.SetFolderMaxSize(folderMaxSize)
+		i.SetLogRotationSettings(folder, folderMaxSize, period)
 	}
 }
