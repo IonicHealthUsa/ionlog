5ffe02ef88e13a3ec936320dfffee458470edb7f
Daniel Jr <daniel.souza.lima@outlook.com>
2025-03-07 20:34:20 -0300

    refactor: Simplify GetCallerInfo functionality

--- internal/logcore/dynamic_fields.go
+++ internal/logcore/dynamic_fields.go
@@ -7,57 +7,27 @@ import (
 	"strings"
 )
 
-func GetCallerInfo(skip int) *callerInfo {
-	var (
-		file     string
-		pkg      string
-		function string
-	)
-
-	var line int
-
-	// Get caller information
-	pc, fullFilePath, line, ok := runtime.Caller(skip)
+func GetCallerInfo(skip int) callerInfo {
+	pc, file, line, ok := runtime.Caller(skip)
 	if !ok {
 		fmt.Fprint(os.Stderr, "Failed to get caller information\n")
-		return nil
+		return callerInfo{}
 	}
+
+	fileLastSlashIndex := strings.LastIndexByte(file, '/')
 
 	// Get function name
-	funcObj := runtime.FuncForPC(pc)
-	if funcObj == nil {
-		fmt.Fprint(os.Stderr, "Failed to get function object\n")
-		return nil
-	}
+	fullFuncName := runtime.FuncForPC(pc).Name()
 
-	fullFuncName := funcObj.Name()
+	lastSlashIndex := strings.LastIndexByte(fullFuncName, '/')
 
-	// Extract package name
-	lastDotIndex := strings.LastIndexByte(fullFuncName, '.')
-	if lastDotIndex < 0 {
-		pkg = ""
-		function = fullFuncName
-	} else {
-		pkg = fullFuncName[:lastDotIndex]
-		function = fullFuncName[lastDotIndex+1:]
+	fistDotIndex := strings.IndexByte(fullFuncName[lastSlashIndex+1:], '.')
+	pkgEnd := lastSlashIndex + 1 + fistDotIndex
 
-		// Get just the last part of the package path
-		if lastSlashIndex := strings.LastIndexByte(pkg, '/'); lastSlashIndex >= 0 {
-			pkg = pkg[lastSlashIndex+1:]
-		}
-	}
-
-	// Extract just the file name from the full path
-	if lastSlashIndex := strings.LastIndexByte(fullFilePath, '/'); lastSlashIndex >= 0 {
-		file = fullFilePath[lastSlashIndex+1:]
-	} else {
-		file = fullFilePath
-	}
-
-	return &callerInfo{
-		File:        file,
-		PackageName: pkg,
-		Function:    function,
+	return callerInfo{
+		File:        file[fileLastSlashIndex+1:],
+		PackageName: fullFuncName[lastSlashIndex+1 : pkgEnd],
+		Function:    fullFuncName[pkgEnd+1:],
 		Line:        line,
 	}
 }
