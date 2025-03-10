6d2e0bbb09947669d717e22e751108d0e3d628b6
Daniel Jr <daniel.souza.lima@outlook.com>
2025-03-10 01:21:48 -0300

    chore(benchmark): removed unusd resetTimer

--- benchmark/basiclogs_test.go
+++ benchmark/basiclogs_test.go
@@ -13,8 +13,6 @@ func BenchmarkBasicLogs(b *testing.B) {
 	ionlog.Start()
 	defer ionlog.Stop()
 
-	b.ResetTimer()
-
 	b.Run("Trace", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			ionlog.Trace(fakeMessage)
