13aba881edb9f8e84a53a5a6b4a333c7f70133dc
Daniel Jr <daniel.souza.lima@outlook.com>
2025-03-10 01:02:22 -0300

    refactor: Recreated logger_settings.go with new services

--- logger_settings.go
+++ logger_settings.go
@@ -5,50 +5,49 @@ import (
 	"io"
 	"os"
 
-	"github.com/IonicHealthUsa/ionlog/internal/ionservice"
-	"github.com/IonicHealthUsa/ionlog/internal/logcore"
-	"github.com/IonicHealthUsa/ionlog/internal/logrotation"
+	"github.com/IonicHealthUsa/ionlog/internal/core/rotationengine"
+	"github.com/IonicHealthUsa/ionlog/internal/service"
 )
 
-type customAttrs func(i logcore.IIonLogger)
+type customAttrs func(i service.ICoreService)
 
 // SetLogAttributes sets the log SetLogAttributes
 // fns is a variadic parameter that accepts customAttrs
 func SetLogAttributes(fns ...customAttrs) {
-	if logcore.Logger().Status() == ionservice.Running {
+	if logger.Status() == service.Running {
 		fmt.Fprint(os.Stderr, "Logger is already running, cannot set attributes\n")
 		return
 	}
 
 	for _, fn := range fns {
-		fn(logcore.Logger())
+		fn(logger)
 	}
 }
 
-// WithTargets sets the write targets for the logger, every log will be written to these targets.
-func WithTargets(w ...io.Writer) customAttrs {
-	return func(i logcore.IIonLogger) {
-		i.SetTargets(w...)
+// WithWriters sets the write targets for the logger, every log will be written to these targets.
+func WithWriters(w ...io.Writer) customAttrs {
+	return func(i service.ICoreService) {
+		i.LogEngine().Writer().SetWriters(w...)
 	}
 }
 
 // WithStaticFields sets the static fields for the logger, every log will have these fields.
 // usage: WithStaicFields(map[string]string{"key": "value", "key2": "value2", ...})
 func WithStaticFields(attrs map[string]string) customAttrs {
-	return func(i logcore.IIonLogger) {
-		i.SetStaticFields(attrs)
+	return func(i service.ICoreService) {
+		i.LogEngine().SetStaticFields(attrs)
 	}
 }
 
 // WithLogFileRotation enables log file rotation, specifying the directory where log files will be stored, the maximum size of the log folder in bytes, and the rotation frequency.
-func WithLogFileRotation(folder string, folderMaxSize uint, period logrotation.PeriodicRotation) customAttrs {
-	return func(i logcore.IIonLogger) {
-		i.SetLogRotationSettings(folder, folderMaxSize, period)
+func WithLogFileRotation(folder string, folderMaxSize uint, period rotationengine.PeriodicRotation) customAttrs {
+	return func(i service.ICoreService) {
+		i.CreateRotationService(folder, folderMaxSize, period)
 	}
 }
 
 func SetReportsBufferSize(size uint) customAttrs {
-	return func(i logcore.IIonLogger) {
-		i.SetReportsBufferSize(size)
+	return func(i service.ICoreService) {
+		i.LogEngine().SetReportQueueSize(size)
 	}
 }
