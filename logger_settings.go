95460fca08603186659a345d24a4db033fb57211
Lucas Moura da Silva <lsilva@ionic.health>
2025-05-06 11:16:31 -0300

    docs: add Go Doc Comments on ionlog package

--- logger_settings.go
+++ logger_settings.go
@@ -25,7 +25,8 @@ func SetAttributes(fns ...customAttrs) {
 	}
 }
 
-// WithWriters sets the write targets for the logger, every log will be written to these targets.
+// WithWriters sets the write targets for the logger,
+// every log will be written to these targets.
 func WithWriters(w ...io.Writer) customAttrs {
 	return func(i service.ICoreService) {
 		i.LogEngine().Writer().SetWriters(w...)
@@ -40,30 +41,34 @@ func WithStaticFields(attrs map[string]string) customAttrs {
 	}
 }
 
-// WithLogFileRotation enables log file rotation, specifying the directory where log files will be stored, the maximum size of the log folder in bytes, and the rotation frequency.
+// WithLogFileRotation enables log file rotation, specifying the directory where log files will be stored,
+// the maximum size of the log folder in bytes, and the rotation frequency.
 func WithLogFileRotation(folder string, folderMaxSize uint, period rotationengine.PeriodicRotation) customAttrs {
 	return func(i service.ICoreService) {
 		i.CreateRotationService(folder, folderMaxSize, period)
 	}
 }
 
+// SetQueueSize sets the size of the reports queue,
+// which stores logs before sending them to a file descriptor.
 func SetQueueSize(size uint) customAttrs {
 	return func(i service.ICoreService) {
 		i.LogEngine().SetReportQueueSize(size)
 	}
 }
 
+// SetTraceMode enables trace log mode.
+// For default, the trace mode is disable,
+// to enable is need pass a true boolean
 func SetTraceMode(mode bool) customAttrs {
 	return func(i service.ICoreService) {
 		i.LogEngine().SetTraceMode(mode)
 	}
 }
 
-// GetOutputStyle return the style of output.
-// When the choice of style is false the return is default
-// stdout, without style.
-// When style is true the return is a custom stdout,
-// with style
+// GetOutputStyle sets the style of output on console.
+// When pass a false boolean, the output is without style.
+// When pass a true boolean, the output is with style
 func GetOutputStyle(style bool) io.Writer {
 	if style {
 		return output.CustomOutput
